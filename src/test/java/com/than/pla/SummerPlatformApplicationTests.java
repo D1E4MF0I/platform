ee274a388ce1ebc3b663148c87069c36dccf334a
Than <2058109198@qq.com>
2023-07-09 21:17:04 +0800

    first commit

---
+++ src/test/java/com/than/pla/SummerPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.than.pla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
