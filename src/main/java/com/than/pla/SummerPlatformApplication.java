7f0f8f3aa7f09823a8d92a74ad3798b77d7a4e90
Than <2058109198@qq.com>
2023-07-09 21:33:39 +0800

    formal commit

--- src/main/java/com/than/pla/SummerPlatformApplication.java
+++ src/main/java/com/than/pla/SummerPlatformApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SummerPlatformApplication {
     public static void main(String[] args) {
         SpringApplication.run(SummerPlatformApplication.class, args);
-        System.out.println("46666");
-        System.out.println("2333");
     }
 
 }
