c8ee6c6eb3a8f08920a31ed9f6116eb2cc6508f4
Than <2058109198@qq.com>
2023-08-08 00:44:09 +0800

    [test]创建远程分支

--- app/src/main/java/than/com/App.java
+++ app/src/main/java/than/com/App.java
@@ -8,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class App {
+    /**
+     *主函数
+     */
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
