ef73e128b769e1090161ec5e799c911859b6c75d
Than <2058109198@qq.com>
2023-08-08 00:12:02 +0800

    [test]分支测试推送

--- app/src/main/java/than/com/App.java
+++ app/src/main/java/than/com/App.java
@@ -3,6 +3,9 @@ package than.com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Than
+ */
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
