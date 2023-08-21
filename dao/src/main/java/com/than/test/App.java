701a54f4459711f928b92799f38bec08d73a4f20
D1E4MF0I <dreamforway@qq.com>
2023-08-21 15:10:53 +0800

    加入测试Dao

--- dao/src/main/java/com/than/test/App.java
+++ dao/src/main/java/com/than/test/App.java
@@ -1,10 +1,12 @@
 package com.than.test;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages={"com.than"},exclude= {DataSourceAutoConfiguration.class})
+
+@SpringBootApplication(scanBasePackages={"com.than"})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
