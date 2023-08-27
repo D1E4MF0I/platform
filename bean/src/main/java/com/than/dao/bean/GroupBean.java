36273461ede0dbb5fa0578defb7e350d2893f0ec
D1EamF01 <840274736@qq.com>
2023-08-28 00:16:41 +0800

    feat: 新增UserBean中name属性;新增GroupBean中group_name属性;新增UserBean，GroupBean自动生成部分唯一属性功能;新增UserBean，GroupBean快速构造函数

--- bean/src/main/java/com/than/dao/bean/GroupBean.java
+++ bean/src/main/java/com/than/dao/bean/GroupBean.java
@@ -8,12 +8,32 @@ import java.sql.Timestamp;
 @Component
 @Data
 public class GroupBean {
-    private String group_account;
+    // 必须
+    private String group_name;
     private String group_creator_account;
-    private Timestamp group_create_time;
-    private String group_sizes;
-    private String group_max_sizes;
+    private Integer group_sizes;
+    private Integer group_max_sizes;
+
     private String group_headshot;
     private String group_background;
     private String group_signature;
+
+    // 自动
+    private String group_account;
+    private Timestamp group_create_time;
+
+
+    public GroupBean() {
+        group_account = String.valueOf(System.currentTimeMillis());
+
+        group_create_time = new Timestamp(System.currentTimeMillis());
+    }
+
+    public GroupBean(String group_name, String group_creator_account, Integer group_sizes, Integer group_max_sizes) {
+        this();
+        this.group_name = group_name;
+        this.group_creator_account = group_creator_account;
+        this.group_sizes = group_sizes;
+        this.group_max_sizes = group_max_sizes;
+    }
 }
--- bean/src/main/java/com/than/dao/bean/UserBean.java
+++ bean/src/main/java/com/than/dao/bean/UserBean.java
@@ -9,11 +9,28 @@ import java.sql.Timestamp;
 @Component
 @Data
 public class UserBean {
-    private String account;
+    // 必须
+    private String name;
     @JsonIgnore
     private String password;
+
     private String headshot;
     private String background;
     private String signature;
+
+    // 自动
+    private String account;
     private Timestamp create_time;
+
+
+    public UserBean() {
+        account = String.valueOf(System.currentTimeMillis());
+        create_time = new Timestamp(System.currentTimeMillis());
+    }
+
+    public UserBean(String name, String password) {
+        this();
+        this.name = name;
+        this.password = password;
+    }
 }
