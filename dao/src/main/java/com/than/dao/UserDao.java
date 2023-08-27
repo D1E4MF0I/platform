b12188949536bb5fe899b6f6dd71e85022a04b01
D1EamF01 <840274736@qq.com>
2023-08-28 00:19:16 +0800

    feat: 新增查询所有账户，根据账户号指定删除功能

--- dao/src/main/java/com/than/dao/UserDao.java
+++ dao/src/main/java/com/than/dao/UserDao.java
@@ -1,26 +1,43 @@
 package com.than.dao;
 
 import com.than.dao.bean.UserBean;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface UserDao {
     /**
-     * 查询 根据账号查询具体账户
+     * 查询 根据账户号查询具体账户
      * @param account 账户号
      * @return UserBean 账户
      */
     @Select("select * from user_tbl where account = #{account}")
     UserBean getByAccount(String account);
 
+
+    /**
+     * 查询 查询所有账户
+     * @return 所有账户的列表
+     */
+    @Select("select * from user_tbl")
+    List<UserBean> getAllUsers();
+
+
     /**
      * 添加 新账户
      * @param userBean
      * @return 操作数量，用于判断操作结果
      */
-    @Insert("INSERT INTO `platformDB`.`user_tbl` (`account`, `password`, `headshot`, `background`, `signature`, `create_time`) VALUES (#{userBean.account}, #{userBean.password}, #{userBean.headshot}, #{userBean.background}, #{userBean.signature}, #{userBean.create_time})")
+    @Insert("INSERT INTO `platformDB`.`user_tbl` (`account`, `password`, `headshot`, `background`, `signature`, `create_time`, `name`) VALUES (#{userBean.account}, #{userBean.password}, #{userBean.headshot}, #{userBean.background}, #{userBean.signature}, #{userBean.create_time}, #{userBean.name})")
     int insertUser(@Param("userBean") UserBean userBean);
+
+
+    /**
+     * 删除 根据账户号指定删除账户
+     * @param account
+     * @return 操作数量，用于判断结果
+     */
+    @Delete("delete from user_tbl where account = #{account}")
+    int deleteUserByAccount(String account);
 }
