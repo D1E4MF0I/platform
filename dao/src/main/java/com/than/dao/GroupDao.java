968289413e18b5733db5daa75ba4921a22a6d531
D1EamF01 <840274736@qq.com>
2023-08-28 00:20:59 +0800

    feat: 新增根据群聊号查询群聊功能; 新增查询全部群聊功能; 新增添加群聊功能; 新增指定群聊号删除群聊功能;

--- dao/src/main/java/com/than/dao/GroupDao.java
+++ dao/src/main/java/com/than/dao/GroupDao.java
@@ -1,8 +1,64 @@
 package com.than.dao;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.than.dao.bean.GroupBean;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface GroupDao {
+    /**
+     * 查询 根据群聊号查询具体群聊
+     *
+     * @param group_account 群聊号
+     * @return GroupBean 群聊
+     */
+    @Select("select * from group_tbl where group_account = #{group_account}")
+    GroupBean getByGroupAccount(String group_account);
 
+
+    /**
+     * 查询 查询所有群聊
+     *
+     * @return 所有群聊的列表
+     */
+    @Select("select * from group_tbl")
+    List<GroupBean> getAllGroups();
+
+
+    /**
+     * 添加 新群聊
+     *
+     * @param groupBean
+     * @return 操作数量，用于判断操作结果
+     */
+    @Insert("INSERT INTO `platformDB`.`group_tbl` (`group_name`," +
+            " `group_account`," +
+            " `group_creator_account`," +
+            " `group_create_time`," +
+            " `group_sizes`," +
+            " `group_max_sizes`," +
+            " `group_headshot`," +
+            " `group_background`," +
+            " `group_signature`) " +
+            "VALUES (#{groupBean.group_name}" +
+            ", #{groupBean.group_account}" +
+            ", #{groupBean.group_creator_account}" +
+            ", #{groupBean.group_create_time}," +
+            " #{groupBean.group_sizes}," +
+            " #{groupBean.group_max_sizes}," +
+            " #{groupBean.group_headshot}," +
+            " #{groupBean.group_background}," +
+            " #{groupBean.group_signature})")
+    int insertUser(@Param("groupBean") GroupBean groupBean);
+
+
+    /**
+     * 删除 根据群聊号删除指定群聊
+     *
+     * @param group_account
+     * @return 操作数量，用于判断结果
+     */
+    @Delete("delete from group_tbl where group_account = #{group_account}")
+    int deleteGroupByGroupAccount(String group_account);
 }
