6f80dc5acd8b5c6439ad471947afaf2eefefd2a4
D1EamF01 <840274736@qq.com>
2023-12-19 21:43:07 +0800

    feat:新增PersonalPostBean具体实现;

--- bean/src/main/java/com/than/controller/bean/PersonalPostBean.java
+++ bean/src/main/java/com/than/controller/bean/PersonalPostBean.java
@@ -2,7 +2,15 @@ package com.than.controller.bean;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class PersonalPostBean {
-
+    private Long id; // 帖子的唯一标识符
+    private String title; // 帖子的标题
+    private String content; // 帖子的内容
+    private Date createTime; // 帖子的创建时间
+    private Date updateTime; // 帖子的最后更新时间
+    private Long authorId; // 发起人的ID
 }
+
