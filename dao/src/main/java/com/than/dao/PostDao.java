cc89e64e5608d596b6f37121d396a7165d5c94cc
D1EamF01 <840274736@qq.com>
2023-12-19 21:43:32 +0800

    feat:新增PostDao具体实现;

--- dao/src/main/java/com/than/dao/PostDao.java
+++ dao/src/main/java/com/than/dao/PostDao.java
@@ -1,6 +1,9 @@
 package com.than.dao;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.than.controller.bean.PersonalPostBean;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 /**
  * @author Than
@@ -9,9 +12,31 @@ import org.apache.ibatis.annotations.Mapper;
  * @description: 操作帖子
  * @date: 2023/10/15 20:52
  */
+
 @Mapper
 public interface PostDao {
+    // 插入帖子
+    @Insert("INSERT INTO posts_tbl (title, content, create_time, update_time, author_id) " +
+            "VALUES (#{title}, #{content}, #{createTime}, #{updateTime}, #{authorId})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insertPersonalPostBean(PersonalPostBean personalPostBean);
 
+    // 根据ID查询帖子
+    @Select("SELECT * FROM posts_tbl WHERE id = #{id}")
+    PersonalPostBean selectPersonalPostBeanById(Long id);
 
+    // 查询所有帖子
+    @Select("SELECT * FROM posts_tbl")
+    List<PersonalPostBean> selectAllPersonalPostBeans();
 
+    // 更新帖子
+    @Update("UPDATE posts_tbl SET title = #{title}, content = #{content}, " +
+            "create_time = #{createTime}, update_time = #{updateTime}, author_id = #{authorId} " +
+            "WHERE id = #{id}")
+    void updatePersonalPostBean(PersonalPostBean personalPostBean);
+
+    // 根据ID删除帖子
+    @Delete("DELETE FROM posts_tbl WHERE id = #{id}")
+    void deletePersonalPostBeanById(Long id);
 }
+
