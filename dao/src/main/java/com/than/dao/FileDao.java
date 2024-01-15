6c61133d38830a1ab36d5b9ad721a21ab562e11a
D1EamF01 <840274736@qq.com>
2024-01-15 10:07:40 +0800

    feat:新增FileDao具体实现;

--- dao/src/main/java/com/than/dao/FileDao.java
+++ dao/src/main/java/com/than/dao/FileDao.java
@@ -1,6 +1,9 @@
 package com.than.dao;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.than.controller.bean.FileBean;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 /**
  * @author Than
@@ -11,7 +14,23 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface FileDao {
+    // 插入操作
+    @Insert("INSERT INTO file_tbl(url, path, time) VALUES(#{url}, #{path}, #{time})")
+    void insert(FileBean file);
 
+    // 删除操作
+    @Delete("DELETE FROM file_tbl WHERE id = #{id}")
+    void deleteById(int id);
 
+    // 更新操作
+    @Update("UPDATE file_tbl SET url = #{url}, path = #{path}, time = #{time} WHERE id = #{id}")
+    void update(FileBean file);
 
+    // 根据ID查询操作
+    @Select("SELECT * FROM file_tbl WHERE id = #{id}")
+    FileBean selectById(int id);
+
+    // 查询所有记录
+    @Select("SELECT * FROM file_tbl")
+    List<FileBean> selectAll();
 }
