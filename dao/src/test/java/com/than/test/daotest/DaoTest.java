e9321bca32b4ea845ba6a44ad73076c8968cbde3
D1EamF01 <840274736@qq.com>
2024-01-15 10:07:50 +0800

    feat:FileDao测试;

--- dao/src/test/java/com/than/test/daotest/DaoTest.java
+++ dao/src/test/java/com/than/test/daotest/DaoTest.java
@@ -1,12 +1,18 @@
 package com.than.test.daotest;
 
 
+import com.than.controller.bean.FileBean;
+import com.than.controller.bean.PersonalPostBean;
+import com.than.dao.FileDao;
 import com.than.dao.GroupDao;
 import com.than.dao.PostDao;
 import com.than.dao.user.UserDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @SpringBootTest
 public class DaoTest {
@@ -18,5 +24,20 @@ public class DaoTest {
 
         System.out.println(userDao.isUserNameExist("2345"));
 
+    }
+
+    @Autowired
+    FileDao fileDao;
+    @Test
+    void testFile(){
+        System.out.println(fileDao.selectById(1));
+        FileBean fileBean = new FileBean();
+        fileBean.setPath("1");
+        fileBean.setTime(String.valueOf(Timestamp.from(Instant.now())));
+        fileBean.setUrl("123");
+        fileDao.insert(fileBean);
+        System.out.println(fileDao.selectAll());
+        fileDao.deleteById(1);
+        System.out.println(fileDao.selectAll());
     }
 }
