5e4db7f8ad03293601a94f3d63d4da835b870e25
D1EamF01 <840274736@qq.com>
2024-01-15 11:09:58 +0800

    feat:新增数据库处理相关Code;

--- bean/src/main/java/com/than/base/Code.java
+++ bean/src/main/java/com/than/base/Code.java
@@ -34,4 +34,16 @@ public class Code {
     public static final int GROUP_AT = 5;
     public static final int PERSON = 6;
 
+    /*帖子数据库处理相关*/
+    public static final int DATABASE_SUCCESS = 50000;
+    public static final int DATABASE_ERROR = 50001;
+    public static final int DATABASE_POST_INSERT_SUCCESS = 50002;
+    public static final int DATABASE_POST_INSERT_ERROR = 50003;
+    public static final int DATABASE_POST_UPDATE_SUCCESS = 50004;
+    public static final int DATABASE_POST_UPDATE_ERROR = 50005;
+    public static final int DATABASE_POST_SELECT_SUCCESS = 50006;
+    public static final int DATABASE_POST_SELECT_ERROR = 50007;
+    public static final int DATABASE_POST_DELETE_SUCCESS = 50008;
+    public static final int DATABASE_POST_DELETE_ERROR = 50009;
+
 }
