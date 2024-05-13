a5b32f50f6cfe4806ae9302cafaba5f447e606f4
DreamFor <dreamforway@qq.com>
2024-05-13 09:42:45 +0800

    feat:新增Local时间树类;

---
+++ bean/src/main/java/com/than/timetree/bean/timetreenode/LocalTimeTreeNode.java
@@ -0,0 +1,16 @@
+package com.than.timetree.bean.timetreenode;
+
+import com.than.timetree.bean.TimeTreeNode;
+import lombok.Data;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Data
+public class LocalTimeTreeNode extends TimeTreeNode {
+    public LocalTimeTreeNode(String local) {
+        this.local = local;
+
+        this.time = Timestamp.from(Instant.now());
+    }
+}
