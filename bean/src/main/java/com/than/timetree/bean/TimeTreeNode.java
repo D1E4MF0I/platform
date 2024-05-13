cf2e7f769fe1a4b57f624e30a24c5b4d73af74b4
DreamFor <dreamforway@qq.com>
2024-05-13 09:00:04 +0800

    perf:优化基类结构;

--- bean/src/main/java/com/than/timetree/bean/TimeTreeNode.java
+++ bean/src/main/java/com/than/timetree/bean/TimeTreeNode.java
@@ -10,29 +10,16 @@ import java.time.LocalTime;
 @Data
 public class TimeTreeNode implements Comparable<TimeTreeNode>{
     // TODO:生成专属ID
-    private Long id;
-    // 指向所对应帖子Id
-    private Long postId;
-    // 该时间节点时间
-    private Timestamp time;
-    // 地点
-    private String local;
+    protected Long id;
 
+
+    // 节点创建时间
+    protected Timestamp time;
     // TODO:发文，操作，去过哪里
 
     // 功能：发帖信息
-    public TimeTreeNode(PersonalPostBean ppb, String local) {
-        this.postId = ppb.getId();
-        this.time = Timestamp.from(ppb.getCreateTime().toInstant());
-        this.local = local;
-    }
 
     // 功能：去过哪里
-    public TimeTreeNode(String local){
-        this.local = local;
-        // 获取时间信息
-        this.time = Timestamp.from(Instant.from(LocalTime.now()));
-    }
 
     // TODO:功能：做过什么操作
 
